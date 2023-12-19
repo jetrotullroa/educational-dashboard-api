39ea0af3fa722c57ee47fe3c3d8b4587c86c42bd
Jetro Tull Roa <jetrotullroa@gmail.com>
2023-12-19 17:40:21 -0500

    fix API names

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
 	path('v1/subjects', views.subjects, name='api-subjects-data'),
 	path('v1/teachers', views.teachers, name='api-teachers-data'),
-	path('v1/teachers/<teacher_id>', views.teacher, name='api-teachers-data'),
-	path('v1/coaches', views.coaches, name='api-subjects-data'),
-	path('v1/coach/<coach_id>', views.coach, name='api-teachers-data'),
-	path('v1/students', views.students, name='api-teachers-data'),
+	path('v1/teachers/<teacher_id>', views.teacher, name='api-teacher-data'),
+	path('v1/coaches', views.coaches, name='api-coaches-data'),
+	path('v1/coach/<coach_id>', views.coach, name='api-coach-data'),
+	path('v1/students', views.students, name='api-students-data'),
 ]
